23fcdd842afcee3e9e03eada96f899be0fdf9afa
wangyi85 <jackywang8911@gmail.com>
2023-10-27 01:29:15 +0800

    BlogDetail Date Format

--- src/components/media/MediaDetail.js
+++ src/components/media/MediaDetail.js
@@ -35,6 +35,11 @@ export default MediaDetail
 
 const Content = memo(props => {
     const navigate = useNavigate();
+    const formatDate = (dateString) => {
+        const options = {year: 'numeric', month: 'long', day: 'numeric'};
+        const date = new Date(dateString);
+        return date.toLocaleDateString('en-US', options);
+    }
     return (
         <div style={styles.container}>
             <Box
@@ -45,7 +50,7 @@ const Content = memo(props => {
                     component={'span'}
                     sx={styles.redTitle}
                 >
-                    {props.content.updatedAt}
+                    {formatDate(props.content.updatedAt)}
                 </Box>
                 <Box
                     component={'span'}

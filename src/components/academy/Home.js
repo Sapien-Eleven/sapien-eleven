4a1f86e11f2e6c68c99acb061842ee8c32f59b3b
wangyi85 <jackywang8911@gmail.com>
2024-03-11 22:32:50 +0800

    Add check whitelist function when entering AcademyPage

--- src/components/academy/Home.js
+++ src/components/academy/Home.js
@@ -8,21 +8,40 @@ import WalletModal from "../WalletModal";
 import SigninModal from "../SigninModal";
 import {useMediaQuery, useTheme} from "@mui/material";
 import {useAccount} from "wagmi";
+import axios from "axios";
+import {SERVER_URL} from "../../const/consts";
+import {enqueueSnackbar} from "notistack";
 
 const Home = memo(props => {
     const [isEntered, setEntered] = useState(false);
-    const {isConnected} = useAccount()
+    const {isConnected, address} = useAccount()
     const [showSigninModal, setShowSigninModal] = useState(false);
     const theme = useTheme();
     const sm = useMediaQuery(theme.breakpoints.down('sm'));
 
-    const onEnter = useCallback(() => {
-        if (isConnected || props.isAuthenticated) {
+    const checkWhitelisted = async () => {
+        if (!isConnected) return;
+        try {
+            const data = (await axios.post(`${SERVER_URL}checkWhitelisted`, {
+                wallet_address: address
+            })).data
+            if (data.status === 'success') return true;
+            else {
+                enqueueSnackbar(data.comment, {variant: 'error'});
+                return false;
+            }
+        } catch (e) {
+            return false;
+        }
+    }
+
+    const onEnter = useCallback(async() => {
+        if (await checkWhitelisted()) {
             setEntered(true);
         }
-    }, [isEntered, isConnected, props.isAuthenticated]);
-    if (sm && (isConnected || props.isAuthenticated) && props.mobileCategory !== null) return <Main mobileCategory={props.mobileCategory} />
-    else if (isEntered && (isConnected || props.isAuthenticated)) return <Main mobileCategory={props.mobileCategory} />
+    }, [isEntered, isConnected]);
+    if (sm && isConnected && props.mobileCategory !== null) return <Main mobileCategory={props.mobileCategory} />
+    else if (isEntered && isConnected) return <Main mobileCategory={props.mobileCategory} />
     else return (
         <div>
             <IntroPanel onPress={onEnter} />
